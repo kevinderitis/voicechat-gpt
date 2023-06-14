3550a0134806a62d8990ad227bf7ee7c3170166e
kevinderitis <kevin.deritis@hotmail.com>
2023-06-14 12:10:14 -0300

    History chat issue solved

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-let messages = [{ role: "user", content: "Sos mi compañero para tareas cotidianas, al proximo mensaje respondeme 'Hola kevin, en que puedo ayudarte?'" }];
+let messages = [
+  {role: "system", content: "Sos mi asistente para todo lo que necesite. Hablas informalmente y de forma muy resumida. Todas las respuestas tienen que tener maximo 10 palabras y no deben perder sentido."}];
 
 const configuration = new Configuration({
   apiKey: process.env.API_KEY
@@ -15,11 +16,20 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const chatGPT = async prompt => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    max_tokens: 100,
-    messages
-  });
+  messages.push({role: "user", content: prompt})
+
+  let response;
+  try {
+    response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      max_tokens: 50,
+      messages
+    });
+  } catch (error) {
+    console.log(error)
+  }
+  
+  
   let respuesta = response["data"]["choices"][0]["message"]["content"];
   messages.push(response["data"]["choices"][0]["message"])
   return respuesta;
